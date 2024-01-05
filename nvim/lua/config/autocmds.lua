9cca46642bd7f21c9b920c842696c0230588e2a1
eduardo paixao <darccau@gmail.com>
2024-01-05 09:09:53 -0300

    feat(nvim): update autocmds

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,89 +1,157 @@
--- Define local variables
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
+local api = vim.api
 
--- Remove trailing whitespaces
-autocmd("BufWritePre", {
+-- don't auto comment new line
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- wrap words "softly" (no carriage return) in mail buffer
+api.nvim_create_autocmd("Filetype", {
+  pattern = "mail",
   callback = function()
-    vim.cmd("%s/\\s\\+$//e")
+    vim.opt.textwidth = 0
+    vim.opt.wrapmargin = 0
+    vim.opt.wrap = true
+    vim.opt.linebreak = true
+    vim.opt.columns = 80
+    vim.opt.colorcolumn = "80"
   end,
 })
 
--- Highlight text on yank
-augroup("YankHighlight", { clear = true })
-autocmd("TextYankPost", {
-  group = "YankHighlight",
+-- Highlight on yank
+api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = "700" })
+    vim.highlight.on_yank()
   end,
 })
 
--- Automatically rebalance windows on vim resize
-autocmd("VimResized", {
+-- go to last loc when opening a buffer
+api.nvim_create_autocmd("BufReadPost", {
   callback = function()
-    vim.cmd("tabdo wincmd =")
-  end,
-})
-
--- Never insert line as a comment when using 'o' to enter insert mode
-autocmd("BufWinEnter", {
-  callback = function()
-    vim.cmd("setlocal formatoptions-=o")
-  end,
-})
-
--- Close man and help with just <q>
-autocmd("FileType", {
-  pattern = { "help", "man", "lspinfo", "checkhealth" },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-  end,
-})
-
--- Auto create dir when saving a file where some intermediate directory does not exist
-autocmd("BufWritePre", {
-  callback = function(event)
-    if event.match:match("^%w%w+://") then
-      return
-    end
-    local file = vim.loop.fs_realpath(event.match) or event.match
-    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-  end,
-})
-
--- Go to the last loc when opening a buffer
-autocmd("BufReadPost", {
-  callback = function()
-    local exclude = { "gitcommit" }
-    local buf = vim.api.nvim_get_current_buf()
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) then
-      return
-    end
-    local mark = vim.api.nvim_buf_get_mark(buf, '"')
-    local lcount = vim.api.nvim_buf_line_count(buf)
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
     if mark[1] > 0 and mark[1] <= lcount then
       pcall(vim.api.nvim_win_set_cursor, 0, mark)
     end
   end,
 })
 
--- Check if the file needs to be reloaded when it's changed
-autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  callback = function()
-    vim.cmd.checktime()
+api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
+
+-- show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = "set cursorline",
+  group = cursorGrp,
+})
+api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+-- Enable spell checking for certain file types
+api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en,de"
+    end,
+  }
+)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)
   end,
 })
 
--- Set cmdheight to 1 when recording, and put it back to normal when it stops
-autocmd("RecordingEnter", {
+vim.api.nvim_create_autocmd("ColorScheme", {
   callback = function()
-    vim.opt_local.cmdheight = 1
+    -- change the background color of floating windows and borders.
+    -- vim.cmd('highlight NormalFloat guibg=none guifg=none')
+    -- vim.cmd('highlight FloatBorder guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight NormalNC guibg=none guifg=none')
+
+    -- vim.cmd('highlight TelescopeBorder guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight TelescopePromptBorder guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight TelescopeResultsBorder guifg=' .. colors.fg .. ' guibg=none')
+    --
+    -- vim.cmd('highlight TelescopePromptTitle guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight TelescopeResultsTitle guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight TelescopePreviewTitle guifg=' .. colors.fg .. ' guibg=none')
+    --
+
+    -- change neotree background colors
+    -- Default: NeoTreeNormal  xxx ctermfg=223 ctermbg=232 guifg=#d4be98 guibg=#141617
+    -- vim.cmd('highlight NeoTreeNormal guibg=#252e33 guifg=none')
+    -- vim.cmd('highlight NeoTreeFloatNormal guifg=none guibg=none')
+    -- vim.cmd('highlight NeoTreeFloatBorder gui=none guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight NeoTreeEndOfBuffer guibg=#252e33') -- 1d2021
+
+    vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+    -- vim.cmd("highlight Comment guifg=#475558")
   end,
 })
 
-autocmd("RecordingLeave", {
-  callback = function()
-    vim.opt_local.cmdheight = 0
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
   end,
+})
+
+-- resize neovim split when terminal is resized
+vim.api.nvim_command("autocmd VimResized * wincmd =")
+
+--fix terraform and hcl comment string
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = "# %s"
+  end,
+  pattern = { "terraform", "hcl" },
+})
+
+-- -- Golang format on save
+-- local goformat_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--     require('go.format').gofmt()
+--   end,
+--   group = goformat_sync_grp,
+-- })
+--
+-- Run gofmt + goimport on save
+local goimport_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    require("go.format").goimport()
+  end,
+  group = goimport_sync_grp,
 })
