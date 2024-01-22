f4a00d0ff1f49108e0aeda40732294fff4b07dac
eduardo paixao <darccau@gmail.com>
2024-01-22 10:55:47 -0300

    feat: add beauty scroll and test automator plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -27,6 +27,18 @@ return {
   },
 
   {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup({
+        stop_eof = true,
+        easing_function = "sine",
+        hide_cursor = true,
+        cursor_scrolls_alone = true,
+      })
+    end,
+  },
+
+  {
     "rcarriga/nvim-notify",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
