8c059bd7c3eb4ea52c4a6e02d23fa11588e1e370
eduardo paixao <darccau@gmail.com>
2024-11-26 11:24:29 -0300

    feat(nvim): reset lazyvim config

--- nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "nvimdev/dashboard-nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/which-key.nvim", enabled = false },
-  { "nvim-lualine/lualine.nvim", enabled = false },
-}
