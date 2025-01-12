215bf375b35e4adaad46b08b659c661da39faaa9
eduardo paixao <darccau@gmail.com>
2025-01-12 15:50:24 -0300

    feat(nvim): enable background transparency on nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     opts = {
       style = "night",
-      transparent = false,
+      transparent = true,
       lualine_bold = true,
       terminal_colors = true,
       on_highlights = function(hl, c)
