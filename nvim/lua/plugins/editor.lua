917f99e2ac86b125f09516932103365898bade96
eduardo paixao <darccau@gmail.com>
2025-05-20 10:07:13 -0300

    feat: add plugins to support codeql and markdown

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -45,4 +45,44 @@ return {
       },
     },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+    opts = {},
+  },
+  {
+    "pwntester/codeql.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/telescope.nvim",
+      "kyazdani42/nvim-web-devicons",
+      {
+        "s1n7ax/nvim-window-picker",
+        version = "v1.*",
+        opts = {
+          autoselect_one = true,
+          include_current = false,
+          filter_rules = {
+            bo = {
+              filetype = {
+                "codeql_panel",
+                "codeql_explorer",
+                "qf",
+                "TelescopePrompt",
+                "TelescopeResults",
+                "notify",
+                "noice",
+                "NvimTree",
+                "neo-tree",
+              },
+              buftype = { "terminal" },
+            },
+          },
+          current_win_hl_color = "#e35e4f",
+          other_win_hl_color = "#44cc41",
+        },
+      },
+    },
+    opts = {},
+  },
 }
