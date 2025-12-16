5796e954018b45551d7d06f4a407d56cdd7c015a
eduardo paixao <darccau@gmail.com>
2025-12-16 15:46:24 -0300

    feat(scripts): set wordlist environment variable in the current shell

--- scripts/__select_wordlist.sh
+++ scripts/__select_wordlist.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-find ~/Documents/SecLists/ -type f ! -path '*/.git/*' | fzf --delimiter / --with-nth -1 | pbcopy
+# Select a wordlist using fzf
+wordlist=$(find ~/Public/SecLists/ -type f ! -path '*/.git/*' |
+  fzf --delimiter / --with-nth -1)
+
+[ -z "$wordlist" ] && exit 0
+
+tmux set-environment -g wordlist "$wordlist"
+
+tmux send-keys "export wordlist=\"$wordlist\"" C-m
